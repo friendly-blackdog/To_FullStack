d327ccb3b7b5d13da20fc3cf7eff04be2d4f18b4
black dog <friendlyblackdog@gmail.com>
2020-04-28 17:15:19 +0900

    add activity properties

--- kotlin/time_fighter/app/src/main/java/com/cash/timefighter/cash410/MainActivity.kt
+++ kotlin/time_fighter/app/src/main/java/com/cash/timefighter/cash410/MainActivity.kt
@@ -2,11 +2,21 @@ package com.cash.timefighter.cash410
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
+
+    internal lateinit var tapMeButton: Button
+    internal lateinit var gameScoreTextView: TextView
+    internal lateinit var timeLeftTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tapMeButton = findViewById(R.id.tapMeButton)
+        gameScoreTextView = findViewById(R.id.gameScoreTextView)
+        timeLeftTextView = findViewById(R.id.timeLeftTextView)
     }
 }
