83230b87c3bd874383c091db79b19419fb88b794
black dog <friendlyblackdog@gmail.com>
2020-04-28 20:15:24 +0900

    startingGame and endGame

--- kotlin/time_fighter/app/src/main/java/com/cash/timefighter/cash410/MainActivity.kt
+++ kotlin/time_fighter/app/src/main/java/com/cash/timefighter/cash410/MainActivity.kt
@@ -3,8 +3,10 @@ package com.cash.timefighter.cash410
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity() {
@@ -22,9 +24,16 @@ class MainActivity : AppCompatActivity() {
     internal var initialCountDown: Long = 60000
     internal var countDownInterval: Long = 1000
 
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "onCreate called. Score is: $score")
+
         tapMeButton = findViewById(R.id.tapMeButton)
         gameScoreTextView = findViewById(R.id.gameScoreTextView)
         timeLeftTextView = findViewById(R.id.timeLeftTextView)
@@ -57,8 +66,22 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun incrementScore() {
+        if(!gameStarted) {
+            startGame()
+        }
+
         score += 1
         val newScore = getString(R.string.yourScore, score)
         gameScoreTextView.text = newScore
+    }
+
+    private fun startGame() {
+        countDownTimer.start()
+        gameStarted = true
+    }
+
+    private fun endGame() {
+        Toast.makeText(this, getString(R.string.gameOverMessage, score), Toast.LENGTH_LONG).show()
+        resetGame()
     }
 }
