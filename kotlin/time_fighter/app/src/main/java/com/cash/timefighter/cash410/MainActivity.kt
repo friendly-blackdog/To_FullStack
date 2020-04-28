df8507129bd93276e15304a11c721df860f38f7d
black dog <friendlyblackdog@gmail.com>
2020-04-28 19:12:11 +0900

    add countdown timer

--- kotlin/time_fighter/app/src/main/java/com/cash/timefighter/cash410/MainActivity.kt
+++ kotlin/time_fighter/app/src/main/java/com/cash/timefighter/cash410/MainActivity.kt
@@ -2,17 +2,25 @@ package com.cash.timefighter.cash410
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.widget.Button
 import android.widget.TextView
 
 
 class MainActivity : AppCompatActivity() {
 
-    internal var score = 0
 
     internal lateinit var tapMeButton: Button
     internal lateinit var gameScoreTextView: TextView
     internal lateinit var timeLeftTextView: TextView
+
+    internal var score = 0
+
+    internal var gameStarted = false
+
+    internal lateinit var countDownTimer: CountDownTimer
+    internal var initialCountDown: Long = 60000
+    internal var countDownInterval: Long = 1000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +33,27 @@ class MainActivity : AppCompatActivity() {
             incrementScore()
         }
 
+        resetGame()
+    }
+
+    private fun resetGame() {
+        score = 0
+
         gameScoreTextView.text = getString(R.string.yourScore, score)
+
+        val initialTimeLeft = initialCountDown / 1000
+        timeLeftTextView.text = getString(R.string.timeLeft, initialTimeLeft)
+
+        countDownTimer = object : CountDownTimer(initialCountDown, countDownInterval) {
+            override fun onTick(millisUntilFinished: Long) {
+                val timeLeft = millisUntilFinished / 1000
+                timeLeftTextView.text = getString(R.string.timeLeft, timeLeft)
+            }
+
+            override fun onFinish() {
+                // To Be Implemented Later
+            }
+        }
     }
 
     private fun incrementScore() {
