92a56b990ca4fd03220f733baadcd5a0f9990fab
black dog <friendlyblackdog@gmail.com>
2020-04-29 20:11:23 +0900

    making it look good
    - add menu
    - app icon and display name

--- kotlin/time_fighter/app/src/main/java/com/cash/timefighter/cash410/MainActivity.kt
+++ kotlin/time_fighter/app/src/main/java/com/cash/timefighter/cash410/MainActivity.kt
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 
 
 class MainActivity : AppCompatActivity() {
@@ -55,6 +58,29 @@ class MainActivity : AppCompatActivity() {
         } else {
             resetGame()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.actionAbout) {
+            showInfo()
+        }
+        return true
+    }
+
+    private fun showInfo() {
+        val dialogTitle = getString(R.string.aboutTitle, BuildConfig.VERSION_NAME)
+        val dialogMessage = getString(R.string.aboutMessage)
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(dialogTitle)
+        builder.setMessage(dialogMessage)
+        builder.create().show()
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
