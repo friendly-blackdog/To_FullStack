f02b51d10bd5dd38944626e57d3dffa74dc11f71
black dog <friendlyblackdog@gmail.com>
2020-05-02 22:58:33 +0900

    login and signup page

---
+++ kotlin/MessengerApp/app/src/main/java/com/cash/messengerapp/cash9541/RegisterActivity.kt
@@ -0,0 +1,27 @@
+package com.cash.messengerapp.cash9541
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+
+class RegisterActivity : AppCompatActivity()
+{
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar_register)
+        setSupportActionBar(toolbar)
+        supportActionBar!!.title = "Register"
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        toolbar.setNavigationOnClickListener {
+            val intent = Intent(this@RegisterActivity, WelcomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+    }
+}
