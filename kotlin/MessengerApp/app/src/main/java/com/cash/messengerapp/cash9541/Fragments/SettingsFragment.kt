939fc0292668d5cc6104c076c64fac7808f1fa29
black dog <friendlyblackdog@gmail.com>
2020-05-04 22:51:07 +0900

    Retrieve & upload image
    Retrieve & upload image to firebase storage & firebase database

--- kotlin/MessengerApp/app/src/main/java/com/cash/messengerapp/cash9541/Fragments/SettingsFragment.kt
+++ kotlin/MessengerApp/app/src/main/java/com/cash/messengerapp/cash9541/Fragments/SettingsFragment.kt
@@ -1,17 +1,28 @@
 package com.cash.messengerapp.cash9541.Fragments
 
 
+import android.app.Activity
+import android.app.ProgressDialog
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.cash.messengerapp.cash9541.ModelClasses.Users
 
 import com.cash.messengerapp.cash9541.R
+import com.google.android.gms.tasks.Continuation
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.*
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.StorageTask
+import com.google.firebase.storage.UploadTask
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.fragment_settings.view.*
 
@@ -23,6 +34,10 @@ class SettingsFragment : Fragment()
 
     var usersReference: DatabaseReference? = null
     var firebaseUser: FirebaseUser? = null
+    private val RequestCode = 438
+    private var imageUri: Uri? = null
+    private var storageRef: StorageReference? = null
+    private var coverChecker: String? = ""
 
 
     override fun onCreateView(
@@ -36,6 +51,7 @@ class SettingsFragment : Fragment()
 
         firebaseUser = FirebaseAuth.getInstance().currentUser
         usersReference = FirebaseDatabase.getInstance().reference.child("Users").child(firebaseUser!!.uid)
+        storageRef = FirebaseStorage.getInstance().reference.child("User Images")
 
 
 
@@ -60,8 +76,91 @@ class SettingsFragment : Fragment()
         })
 
 
+        view.profile_image_settings.setOnClickListener{
+            pickImage()
+        }
+
+        view.cover_image_settings.setOnClickListener{
+            coverChecker = "cover"
+            pickImage()
+        }
+
 
         return view
+    }
+
+    private fun pickImage()
+    {
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(intent, RequestCode)
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+
+        if (requestCode == RequestCode && resultCode == Activity.RESULT_OK && data!!.data != null)
+        {
+            imageUri = data.data
+            Toast.makeText(context, "uploading...", Toast.LENGTH_LONG).show()
+            uploadImageToDatabase()
+        }
+    }
+
+    private fun uploadImageToDatabase()
+    {
+        val progressBar = ProgressDialog(context)
+        progressBar.setMessage("image is uploading, please wait...")
+        progressBar.show()
+
+        if (imageUri != null)
+        {
+            val fileRef = storageRef!!.child(System.currentTimeMillis().toString() + ".jpg")
+
+            var uploadTask: StorageTask<*>
+            uploadTask = fileRef.putFile(imageUri!!)
+
+            uploadTask.continueWith(Continuation <UploadTask.TaskSnapshot, Task<Uri>>{ task ->
+                if (!task.isSuccessful)
+                {
+                    task.exception?.let {
+                        throw it
+                    }
+                }
+                return@Continuation fileRef.downloadUrl
+            }).addOnCompleteListener { task ->
+                if (task.isSuccessful)
+                {
+                    val downloadUrl = task.result
+                    val url = downloadUrl.toString()
+
+
+                    if (coverChecker == "cover")
+                    {
+                        val mapCoverImg = HashMap<String, Any>()
+                        mapCoverImg["Cover"] = url
+                        usersReference!!.updateChildren(mapCoverImg)
+                        coverChecker = ""
+
+
+
+                    }
+                    else
+                    {
+                        val mapProfileImg = HashMap<String, Any>()
+                        mapProfileImg["profile"] = url
+                        usersReference!!.updateChildren(mapProfileImg)
+                        coverChecker = ""
+                    }
+                    progressBar.dismiss()
+                }
+            }
+
+
+        }
     }
 
 
