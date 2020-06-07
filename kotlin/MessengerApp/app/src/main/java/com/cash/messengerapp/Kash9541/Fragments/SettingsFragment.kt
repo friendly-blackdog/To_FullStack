cf5fda003a051f793890a11b7ceedda3bbfebaca
black dog <friendlyblackdog@gmail.com>
2020-06-07 12:07:18 +0900

    cover & profile image
    UI for account settings fragment for cover image and profile image.
    retrieve and display default cover photo and profile image on settings fragment.

--- kotlin/MessengerApp/app/src/main/java/com/cash/messengerapp/Kash9541/Fragments/SettingsFragment.kt
+++ kotlin/MessengerApp/app/src/main/java/com/cash/messengerapp/Kash9541/Fragments/SettingsFragment.kt
@@ -7,8 +7,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.cash.messengerapp.Kash9541.ModelClasses.Users
 
 import com.cash.messengerapp.Kash9541.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.*
+import com.google.firebase.ktx.Firebase
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.fragment_settings.view.*
 
 /**
  * A simple [Fragment] subclass.
@@ -16,14 +23,48 @@ import com.cash.messengerapp.Kash9541.R
  * [SettingsFragment.OnFragmentInteractionListener] interface
  * to handle interaction events.
  */
-class SettingsFragment : Fragment() {
+class SettingsFragment : Fragment()
+{
+    var usersReference: DatabaseReference? = null
+    var firebaseUser: FirebaseUser? = null
+
+
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        val view = inflater.inflate(R.layout.fragment_settings, container, false)
+
+
+        firebaseUser = FirebaseAuth.getInstance().currentUser
+        usersReference = FirebaseDatabase.getInstance().reference.child("User").child(firebaseUser!!.uid)
+
+        usersReference!!.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(p0: DataSnapshot) {
+                if (p0.exists())
+                {
+                    val user: Users? = p0.getValue(Users::class.java)
+
+
+                    if (context != null)
+                    {
+                        view.username_settings.text = user!!.getUserName()
+                        Picasso.get().load(user.getProfile()).into(view.profile_image_settings)
+                        Picasso.get().load(user.getCover()).into(view.cover_image_settings)
+                    }
+                }
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+
+            }
+        })
+
+        return view
     }
 
 }
